3296dc9541fc8d45f657eb22c5ec0c5e9353345b
JOIN <pj5807@naver.com>
2022-05-07 17:19:39 +0900

    add result file save func

--- trainer.py
+++ trainer.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.distributions as td
+import pandas as pd
 from simulator import Simulator
 from models import POMDPModel
 from buffer import Buffer
@@ -42,12 +43,24 @@ class Trainer:
                                  self._wm_lr, self._actor_lr, self._value_lr, self._lambda, self._actor_entropy_scale,
                                  self._discount)
         self._set_model()
+        self._train_metrics = {'wm_loss':[], 'a_loss':[], 'v_loss':[]}
+        self._result = {'reward': [0], 'accumulated_reward': [0]}
 
     def _set_model(self):
         if Path(self._model_path).exists():
             self._pomdp.load_model(self._model_path)
         else:
             self._pomdp.save_model(self._model_path)
+
+    def save_train_metrics(self):
+        df = pd.DataFrame(self._train_metrics)
+        path = Path(__file__).parent.resolve() / 'Results' / 'train_metrics.csv'
+        df.to_csv(path, encoding='cp949')
+
+    def save_result(self):
+        df = pd.DataFrame(self._result)
+        path = Path(__file__).parent.resolve() / 'Results' / 'train_result.csv'
+        df.to_csv(path, encoding='cp949')
 
     def add_to_buffer(self, observ, action, reward):
         self._buffer.add(observ, action, reward)
@@ -59,7 +72,7 @@ class Trainer:
         self._simulator.reset()
         self._pomdp.reset(initial_action=self._simulator.sample_action())
 
-    def step(self, num_steps = 1):
+    def step(self, num_steps = 1, train = False):
         action = self._pomdp.prev_action
         observ_list = []
         reward_list = []
@@ -70,21 +83,27 @@ class Trainer:
             observ_list.append(observ)
             reward_list.append(reward)
             action_list.append(action)
+            if train:
+                self._result['reward'].append(reward)
+                self._result['accumulated_reward'].append(self._result['accumulated_reward'][-1] + reward)
         return observ_list, reward_list, action_list
 
     def train(self, observ, action, reward, num_step):
         observ = torch.tensor(observ, dtype=torch.float32).to(self._device)
         action = torch.tensor(action, dtype=torch.float32).to(self._device)
         reward = torch.tensor(reward, dtype=torch.float32).to(self._device)
+        wm_loss = []
+        a_loss = []
+        v_loss = []
 
         # Dynamics learning
         for step in range(1, num_step + 1):
             rnn_hidden, kld_loss, observ_loss, model_loss, reward_loss = self._pomdp.world_model_loss(observ, action, reward)
-
             self._pomdp.world_model_optimizer.zero_grad()
             model_loss.backward()
             torch.nn.utils.clip_grad_norm_(self._pomdp.world_model_parameters, max_norm=0.1)
             self._pomdp.world_model_optimizer.step()
+            wm_loss.append(model_loss.item())
 
             if step % 10 == 0:
                 print(f"{step}/{num_step}  ", f"Model loss: {model_loss.item():.6f} ",
@@ -104,10 +123,12 @@ class Trainer:
             self._pomdp.value_model_optimizer.zero_grad()
             actor_loss.backward()
             value_loss.backward()
-            torch.nn.utils.clip_grad_norm_(self._pomdp.actor_parameters, 1)
-            torch.nn.utils.clip_grad_norm_(self._pomdp.value_model_parameters, 0.1)
+            torch.nn.utils.clip_grad_norm_(self._pomdp.actor_parameters, max_norm=1)
+            torch.nn.utils.clip_grad_norm_(self._pomdp.value_model_parameters, max_norm=1)
             self._pomdp.actor_optimizer.step()
             self._pomdp.value_model_optimizer.step()
+            a_loss.append(actor_loss.item())
+            v_loss.append(value_loss.item())
 
             if step % self._update_interval == 0:
                 self._pomdp.update_target_value(self._value_mix_rate)
@@ -119,6 +140,10 @@ class Trainer:
                 # imagine test
                 o, a, r = self._pomdp.imagine_test(rnn_hidden, self._horizon)
                 print(f"imagine test \nobserv:{o[:1]} \naction:{a[:1]} \nreward:{r[:1]}")
+
+        self._train_metrics['wm_loss'].append(np.mean(wm_loss))
+        self._train_metrics['a_loss'].append(np.mean(a_loss))
+        self._train_metrics['v_loss'].append(np.mean(v_loss))
 
 
 if __name__ == '__main__':
@@ -133,7 +158,7 @@ if __name__ == '__main__':
     parser.add_argument('--rnn_input_size', type=int, default=8, help='RNN input size')
     parser.add_argument('--rnn_hidden_size', type=int, default=64, help='RNN hidden size')
     parser.add_argument('--wm_lr', type=float, default=0.003, help='World model Learning rate')
-    parser.add_argument('--actor_lr', type=float, default=0.00005, help='Action model Learning rate')
+    parser.add_argument('--actor_lr', type=float, default=0.00001, help='Action model Learning rate')
     parser.add_argument('--value_lr', type=float, default=0.0005, help='Value model Learning rate')
     parser.add_argument('--horizon', type=int, default=100, help='Horizon length')
     parser.add_argument('--lambda_', type=float, default=0.9, help='TD lambda')
@@ -159,13 +184,12 @@ if __name__ == '__main__':
         trainer.train(observ, action, reward, 1000)
 
         # environment interaction
-        obs, rew, act = trainer.step(1000)
+        obs, rew, act = trainer.step(10, train=True)
         print(f"environment interaction")
         print(f"observ : {np.argmax(obs, axis= -1)}")
         print(f"action : {np.argmax(act, axis= -1)}")
         trainer.add_to_buffer(obs, act, rew)
 
-
-
-
-
+    trainer.save_train_metrics()
+    trainer.save_result()
+    print("fin")
